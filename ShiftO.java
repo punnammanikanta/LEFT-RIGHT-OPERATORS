fb9767c3db796b79b4b5c00c312c756b4d1d2677
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 12:10:44 +0530

    Add files via upload

---
+++ ShiftO.java
@@ -0,0 +1,16 @@
+public class ShiftO {
+    
+        public static void main(String[] args) {
+            int a = 8;
+            int result = a << 2;
+            System.out.printf("The left shift of %d is: %d", a, result);
+    
+            int b = 20;
+            int res = b >> 2; 
+            System.out.printf("\nThe right shift of %d is : %d", b, res); 
+        }
+    }
+    
+
+    
+
